4c9ac5ca54594d365995a3e03bf3b2a877712b9d
Jujstme <just.tribe@gmail.com>
2019-09-30 19:40:32 +0200

    Bug fixing

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace TsrNoAi
 		private static extern bool WriteProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpbuffer, int nSize, int lpNumberOfBytesWritten);
 		
         private static int processHandle;
+		
+		private int ModuleMemorySize { get; set;}
 
 		public static void Main()
 		{
@@ -31,8 +33,16 @@ namespace TsrNoAi
                     Environment.Exit(2);
                 }
 				
-				WriteProcessMemory(processHandle, 0x145B2A1FD, new byte[] {0xC7, 0x80, 0x04, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x90}, 11, 0);
+				if (process.MainModule.ModuleMemorySize != 367644672)
+				{
+					MessageBox.Show("Cannot apply patch. Please ensure you are\n" +
+									"running the correct version of the game!", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Environment.Exit(2);
+                }
+				
 				WriteProcessMemory(processHandle, 0x140C0AC80, new byte[] {0x00, 0x00, 0x00, 0x00}, 4, 0);
+				WriteProcessMemory(processHandle, 0x145B2A1FD, new byte[] {0xE9, 0x19, 0x29, 0x00, 0x00, 0x90}, 6, 0);
+				WriteProcessMemory(processHandle, 0x145B2CB1B, new byte[] {0xC7, 0x80, 0x04, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE9, 0xD9, 0xD6, 0xFF, 0xFF}, 15, 0);
 			}
 			
 			 if (processList.Length > 0)
