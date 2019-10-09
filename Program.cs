b04c0b5aaa8be6cc405125469015524ae0a5d7d2
Jujstme <just.tribe@gmail.com>
2019-10-09 02:37:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,11 +14,11 @@ namespace TsrNoAi
 		private static extern bool WriteProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpbuffer, int nSize, int lpNumberOfBytesWritten);
 		
 		[DllImport("kernel32.dll")]
-		private static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, int lpNumberOfBytesRead);
+		private static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int nSize, int lpNumberOfBytesRead);
 		
         private static int processHandle;
 		
-		public static void Main(string[] args)
+		public static void Main()
 		{
 			Application.EnableVisualStyles();
 			
@@ -41,39 +41,59 @@ namespace TsrNoAi
                     Environment.Exit(2);
                 }
 				
-				
-				byte[] buffer = new byte[8];
-				int bytesRead = 0;
-				
-				ReadProcessMemory(processHandle, 0x14102DE40, buffer, buffer.Length, bytesRead);
-				Int64 baseValue = BitConverter.ToInt64(buffer, 0);
-				baseValue = baseValue + 0x340;
-				ReadProcessMemory(processHandle, baseValue, buffer, buffer.Length, bytesRead);
-				baseValue = BitConverter.ToInt64(buffer, 0);
-				
-				if (baseValue == 2) {
-					WriteProcessMemory(processHandle, 0x140C0AC80, new byte[] {0x00, 0x00, 0x00, 0x00}, 4, 0);
+				if (processList.Length == 0)
+				{
+					MessageBox.Show("Please start the game first!", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					Environment.Exit(0);
 				}
-				WriteProcessMemory(processHandle, 0x145B2A1FD, new byte[] {0xE8, 0x19, 0x29, 0x00, 0x00, 0x90}, 6, 0);
-				WriteProcessMemory(processHandle, 0x145B2CB1B, new byte[] {0x48, 0x8B, 0x1D, 0x1E, 0x13, 0x50, 0xFB, 0x83, 0xBB, 0x40, 0x03, 0x00, 0x00, 0x02, 0x89, 0x88, 0x04, 0x02, 0x00, 0x00, 0x75, 0x0A, 0xC7, 0x80, 0x04, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC3}, 33, 0);
-			}
-			
-			 if (processList.Length > 0)
-            {
-                MessageBox.Show(
-                    "In exhibition races (standard race) you are now\n" +
-                    "free from AI players!!!\n\n" +
-                    "To restore the default behaviour, " +
-					"just restart the game.\n\n" +
+				
+				byte[] status = new byte[6];
+				int bytesRead = 0;
+				ReadProcessMemory(processHandle, 0x145B2A1FD, status, status.Length, bytesRead);
+
+				if (BitConverter.ToString(status, 0) == "89-88-04-02-00-00")
+				{
+					// Code gets applied if the application determines the game is unpatched
+					byte[] buffer = new byte[8];
+					ReadProcessMemory(processHandle, 0x14102DE40, buffer, buffer.Length, bytesRead);
+					Int64 baseValue = BitConverter.ToInt64(buffer, 0);
+					baseValue = baseValue + 0x340;
+					buffer = new byte[8];
+					ReadProcessMemory(processHandle, baseValue, buffer, 4, bytesRead);
+					baseValue = BitConverter.ToInt64(buffer, 0);
+					
+					if (baseValue == 2) {
+						WriteProcessMemory(processHandle, 0x140C0AC80, new byte[] {0x00, 0x00, 0x00, 0x00}, 4, 0);
+					}
+					WriteProcessMemory(processHandle, 0x145B2A1FD, new byte[] {0xE8, 0x19, 0x29, 0x00, 0x00, 0x90}, 6, 0);
+					WriteProcessMemory(processHandle, 0x145B2CB1B, new byte[] {0x48, 0x8B, 0x1D, 0x1E, 0x13, 0x50, 0xFB, 0x83, 0xBB, 0x40, 0x03, 0x00, 0x00, 0x02, 0x89, 0x88, 0x04, 0x02, 0x00, 0x00, 0x75, 0x0A, 0xC7, 0x80, 0x04, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC3}, 33, 0);
+					
+					MessageBox.Show(
+					"In exhibition races (standard race) you are now\n" +
+					"free from AI players!!!\n\n" +
+					"To restore the default behaviour, either restart the game\nor launch this tool again!\n\n" +
 					"Note: this tool does not apply to team races!\n\n"+
-                    "Enjoy! :)", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Please start the game first!", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(0);
-            }
+					"Enjoy! :)", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				else
+				{
+					// Unpatching the game is the no AI patch was already applied
+					WriteProcessMemory(processHandle, 0x140C0AC80, new byte[] {0xFF, 0xFF, 0xFF, 0xFF}, 4, 0);
+					WriteProcessMemory(processHandle, 0x145B2A1FD, new byte[] {0x89, 0x88, 0x04, 0x02, 0x00, 0x00}, 6, 0);
+					
+					byte[] buffer = new byte[33];
+					for (int index = 0; index < buffer.Length; ++index)
+					{
+						buffer[index] = 0x00;
+					}
+					WriteProcessMemory(processHandle, 0x145B2CB1B, buffer, 33, 0);
+					
+					MessageBox.Show(
+					"TSR NO AI patch has been removed!\n" +
+					"Enjoy your stock experience!", "TSR NO AI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				
+			}
         }
-	 
     }
 }
